00cdd89237040ff3c555fadb59e43f5caa68c43d
abhishekwarrior <abhishekjadhav0204@gmail.com>
2025-04-29 07:37:10 +0000

    datatypes

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,14 @@
-console.log("Abhishek");
+const accountId = 125444;
+let accountEmail = "abhishekjadhav8233@gmail.com";
+var accountPassword = "1251";
+accountCity = "jaipur";
+let accountState;
+
+//accountId = 2;
+
+accountEmail = "jaabhishek022@gmail.com";
+accountPassword = "12548";
+accountCity = "channi";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
