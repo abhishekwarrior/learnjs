48c08216a1d7d1b530179bde26dd0d044bdbd351
abhishekwarrior <abhishekjadhav0204@gmail.com>
2025-04-29 07:32:20 +0000

    datatypes

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,12 @@
+"use strict" // treat all js code as newer version
+//alert(3 + 3); we are using nodeJS not browser
+
+console.log(3 + 3);
+
+console.log("abhishek");
+
+let name = "abhishek"
+let age = 23;
+let isLoggedIN = false;
+
+console.table([name,age, isLoggedIN]);
